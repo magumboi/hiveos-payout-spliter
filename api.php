9badffba0299b5dc60422cb6817574297884dc72
Iván Caldera <magumboss1@gmail.com>
2021-11-29 23:32:37 -0600

    Update api.php

--- api.php
+++ api.php
@@ -9,46 +9,44 @@ use League\Csv\Statement;
 header('Content-Type: application/json; charset=utf-8');
 
 if (isset($_POST['file']) && isset($_POST['income']) && isset($_POST['payday']) && isset($_POST['workedDays'])) {
-    //The resource that we want to download.
-    $fileUrl = $_POST['file'];
+    main($_POST['income'],$_POST['payday'],$_POST['workedDays'],$_POST['isOwner']);
+    /* //The resource that we want to download.
+     $fileUrl = $_POST['file'];
 
-    $filesaveTo = 'stats.csv';
+     $filesaveTo = 'stats.csv';
+ 
+     $fp = fopen($filesaveTo, 'w+');
 
-    $fp = fopen($filesaveTo, 'w+');
+     if($fp === false){
+         throw new Exception('Could not open: ' . $filesaveTo);
+     }
 
-    if($fp === false){
-        throw new Exception('Could not open: ' . $filesaveTo);
-    }
+     $ch = curl_init($fileUrl);
+     curl_setopt($ch, CURLOPT_FILE, $fp);
+     curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+     curl_exec($ch);
 
-    $ch = curl_init($fileUrl);
-    curl_setopt($ch, CURLOPT_FILE, $fp);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 20);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_exec($ch);
+     if(curl_errno($ch)){
+         throw new Exception(curl_error($ch));
+     }
 
-    if(curl_errno($ch)){
-        throw new Exception(curl_error($ch));
-    }
-
-    $getstatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    curl_close($ch);
-    fclose($fp);
-    if($getstatusCode == 200){
-        //echo 'Downloaded!';
-        main($_POST['income'],$_POST['payday'],$_POST['workedDays'],$_POST['isOwner']);
-    } else{
-        echo "Status Code: " . $getstatusCode;
-    }
+     $getstatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+     curl_close($ch);
+     fclose($fp);
+     if($getstatusCode == 200){
+         //echo 'Downloaded!';
+     } else{
+         echo "Status Code: " . $getstatusCode;
+     }*/
 }
 
 function main($income,$payday,$workedDays,$isOwner){
 
     //$payday = date("d/m/Y");
 
-    $payDayFormat = DateTime::createFromFormat('d/m/Y',$payday);
-    $contractDateEnd = date('Y-m-d', strtotime($payDayFormat->format('Y-m-d')));
-    $payDayFormat->modify('-'.$workedDays.' day');
-    $contractDateBegin = date('Y-m-d', strtotime($payDayFormat->format('Y-m-d')));
+    $contractDateEnd = date('Y-m-d', strtotime(DateTime::createFromFormat('d/m/Y',$payday)->modify('-1 day')->format('Y-m-d')));
+    $contractDateBegin = date('Y-m-d', strtotime(DateTime::createFromFormat('d/m/Y',$payday)->modify('-'.$workedDays.' day')->format('Y-m-d')));
 
     $resultado = array();
     $prom = array();
@@ -59,27 +57,35 @@ function main($income,$payday,$workedDays,$isOwner){
     $daycont = 0;
     $dead = 0;
     try {
-        $reader = Reader::createFromPath('stats.csv', 'r');
+        /*$reader = Reader::createFromPath('stats.csv', 'r');
         $reader->setHeaderOffset(0);
         $records = Statement::create()->process($reader);
         $gpus = substr_count(implode(" ",$records->getHeader()), "Unit");
         $recordcount = count($reader);
         $json = json_encode($reader);
-        $obj = json_decode($json);
+        $obj = json_decode($json);*/
+
+        $dsn = "mysql:host=localhost;dbname=hiveos";
+        $user = "root";
+        $passwd = "lolazo34";
+        $pdo = new PDO($dsn, $user, $passwd);
+        $query = "SELECT * FROM `metrics` WHERE `server_time` BETWEEN '".$contractDateBegin." 00:00:00' AND '".$contractDateEnd." 23:55:00' ORDER BY `server_time`";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute();
+        $rows = $stmt->fetch();
+        $rowCount = $stmt->rowCount();
+        $gpus = count(explode(',',$rows[0]['ethash']));
+
         for ($j = 1; $j <= $gpus; $j++) {
-            for ($i = 0; $i < $recordcount; $i++) {
-                if(substr_count($obj[$i]->{'time'}, ":") == 2)
-                    $hashtime = DateTime::createFromFormat('d/m/Y H:i:s', $obj[$i]->{'time'});
+            for ($i = 0; $i < $rowCount; $i++) {
+                $hashrates = explode(',',$rows[0]['ethash']);
+                $gpuHr = $hashrates[$j];
+                $daycont++;
+                $sum = $sum + $gpuHr;
+                if ($gpuHr != 0)
+                    $zerocont++;
                 else
-                    $hashtime = DateTime::createFromFormat('d/m/Y H:i', $obj[$i]->{'time'});
-                if ((date('Y-m-d', strtotime($hashtime->format('Y-m-d'))) >= $contractDateBegin) && (date('Y-m-d', strtotime($hashtime->format('Y-m-d'))) < $contractDateEnd)) {
-                    $daycont++;
-                    $sum = $sum + $obj[$i]->{'Unit ' . $j . ' ethash H/s'};
-                    if ($obj[$i]->{'Unit ' . $j . ' ethash H/s'} != 0)
-                        $zerocont++;
-                    else
-                        $dead++;
-                }
+                    $dead++;
             }
             if ($daycont != 0) {
                 $sumavg = $sumavg + ($sum / $daycont);
@@ -92,6 +98,7 @@ function main($income,$payday,$workedDays,$isOwner){
             $daycont = 0;
             $dead = 0;
         }
+
         $resultado['gpuhs'] = $prom;
         $resultado['dead'] = $adead;
         $resultado['income'] = $income;
@@ -105,7 +112,7 @@ function main($income,$payday,$workedDays,$isOwner){
         }
         $resultado['gpuIncome'] =  $ai;
 
-         $resultado['respuesta'] = true;
+        $resultado['respuesta'] = true;
 
         /*
          if($isOwner){
